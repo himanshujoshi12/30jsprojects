0ab0bc9802375fd53f5a9d089b0c51cdb342ae72
Himanshu Joshi <himanshujoshi02001@gmail.com>
2023-10-08 20:19:49 +0530

    bgChanger

---
+++ Project2/script.js
@@ -0,0 +1,12 @@
+function redColor(){
+    document.body.style.backgroundColor="red";
+}
+function greenColor(){
+    document.body.style.backgroundColor="green";
+}
+function purpleColor(){
+    document.body.style.backgroundColor = "purple";
+}
+function orangeColor(){
+    document.body.style.backgroundColor = "orange";
+}
